bfeeef1c28a0ad35ccd78c0b4b7cdd8c850dd31f
Vignesh Shanmugam <vignesh.shanmugam22@gmail.com>
2015-12-17 18:07:37 +0100

    Deploy script changes

--- gh-pages.sh
+++ gh-pages.sh
@@ -6,7 +6,17 @@ mkdir gh-pages;
 
 # compile the files using Elm
 elm make Main.elm --output gh-pages/main.html
-html-minifier --minify-js --minify-js gh-pages/main.html -o gh-pages/index.html
+html-minifier --minify-css --minify-js gh-pages/main.html -o gh-pages/index.html
+rm gh-pages/main.html
 
 # Copy the images
 cp -R img gh-pages
+
+cd gh-pages
+
+git init
+git config user.name "travis"
+git add .
+git commit -m "Deploying to GH Pages"
+
+git push --force "https://${GH_TOKEN}@github.com/zalando/elm-street-404.git" master:gh-pages > /dev/null 2>&1
