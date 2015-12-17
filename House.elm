e4ee1498f5c11f2c48738990a4cf961f9e066336
Arpad Ryszka <arpad.ryszka@zalando.de>
2015-12-17 22:31:15 +0100

    add timeouting requests

--- House.elm
+++ House.elm
@@ -4,6 +4,7 @@ module House (House, house) where
 type alias House =
   { position : (Float, Float)
   , size : (Float, Float)
+  , happiness : Int
   }
 
 
@@ -11,4 +12,5 @@ house : (Float, Float) -> House
 house position =
   { position = position
   , size = (3, 2)
+  , happiness = 1
   }
