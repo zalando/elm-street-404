2d5b41794581c8c85d50673d122b9fd81a7a5612
Vignesh Shanmugam <vignesh.shanmugam22@gmail.com>
2015-12-17 17:37:54 +0100

    Adding html-minifier

--- gh-pages.sh
+++ gh-pages.sh
@@ -5,7 +5,8 @@ rm -rf gh-pages || exit 0;
 mkdir gh-pages;
 
 # compile the files using Elm
-elm make Main.elm --output gh-pages/index.html
+elm make Main.elm --output gh-pages/main.html
+html-minifier --minify-js --minify-js gh-pages/main.html -o gh-pages/index.html
 
 # Copy the images
 cp -R img gh-pages
