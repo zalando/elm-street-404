72a44feddb50c8fc651ae9be0c1cb62ebd5cfcd6
Andrey Kuzmin <unsoundscapes@gmail.com>
2016-07-19 11:56:02 +0200

    Made customers clickable

--- src/CustomerView.elm
+++ src/CustomerView.elm
@@ -8,6 +8,7 @@ import MapObject exposing (MapObject)
 import Request exposing (Request)
 import Article exposing (Article)
 import Category exposing (Category)
+import Actions
 
 
 shirtFrameOffset : Customer -> Int -> Int
@@ -52,4 +53,6 @@ render requests articles ({position} as house) customer =
       renderColor pantsColor 4 Textures.Trousers ++
       renderColor shoesColor 3 Textures.Shoes ++
       renderColor shirtColor 2 Textures.Shirts ++
-      [ Box.textured Textures.Customers position (customerFrameOffset customer) (layers.obstacle, 1) ]
+      [ Box.textured Textures.Customers position (customerFrameOffset customer) (layers.obstacle, 1)
+      , Box.clickable (2, 3) (0, 0) position (layers.click, 0) (Actions.ClickMapObject house)
+      ]
